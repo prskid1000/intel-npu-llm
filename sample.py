e84a83ade4c85b5a04899bc18272048e1d6458cd
Prithwiraj Samanta <prskid1000@gmail.com>
2025-10-15 09:35:55 +0530

    updates

---
+++ sample.py
@@ -0,0 +1,14 @@
+def main():
+    import openvino_genai as ov_genai
+
+    import openvino as ov
+    core = ov.Core()
+    print("Available devices:", core.available_devices)
+    
+    model_path = "./models/Qwen/Qwen2.5-3B"
+    pipe = ov_genai.LLMPipeline(model_path, "NPU")
+    print(pipe.generate("The Sun is yellow because", max_new_tokens=100))
+
+
+if '__main__' == __name__:
+    main()
